a0c8038a1e88ffa7cdbb78dd117d5a49a6d2f1e1
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2019-12-26 13:33:00 +0900

    :heavy_plus_sign: 現時点のスクリプトを入れた

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('Hello World!');
+const _ = require('lodash');
+const csv = require('csv');
+const fs = require('fs');
+const util = require('util');
+
+const readFileAsync = util.promisify(fs.readFile);
+const parseCsvAsync = util.promisify(csv.parse);
+
+type MatrixCellData = string | number | boolean | null;
+type MatrixRowData = MatrixCellData[];
+type MatrixData = MatrixRowData[];
+
+const createEmptyMatrix = (width: number, height: number, initialValue = null) => _.times(height, () => new Array(width).fill(initialValue));
+const appendMatrixHeader = (matrix: MatrixData, X: MatrixCellData[], Y: MatrixCellData[]) => [[null, ...X], ...matrix.map((it, idx) => [Y[idx], ...it])];
+
+const linesToMatrix = (lines: string[], xPos = 0, yPos = 1, vPos = 2) => {
+  const [X, Y] = [_.uniq(_.map(lines, xPos)), _.uniq(_.map(lines, yPos))];
+  const matrix = lines.reduce((m, it) => (m[Y.indexOf(it[yPos])][X.indexOf(it[xPos])] = it[vPos], m), createEmptyMatrix(X.length, Y.length));
+  return appendMatrixHeader(matrix, X, Y);
+};
+
+(async function main() {
+  const path = process.argv[2];
+  const content = await readFileAsync(path, 'utf8');
+  const csvLines = await parseCsvAsync(content);
+  const csvLinesSorted = _.orderBy(csvLines, [0, 1])
+  const matrix = linesToMatrix(csvLinesSorted, 0, 1, 4);
+  console.log(matrix.map((it) => it.join(",")).join("\n"));
+})();
