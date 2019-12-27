b5c591d175149efd8acc4d5b6ff00ca62529e90a
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2019-12-27 13:37:15 +0900

    :heavy_plus_sign: コマンド定義した

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,28 @@ import stringify from "csv-stringify";
 import parse from "csv-parse";
 import fs from "fs";
 import util from "util";
+import commander from "commander";
 import { MatrixBuilder } from "./matrix_builder";
 
 const readFileAsync = util.promisify(fs.readFile);
 const parseCsvAsync = util.promisify<any, any>(parse);
 const stringifyCsvAsync = util.promisify<any, any>(stringify);
 
-(async function main() {
+const program = new commander.Command();
+program
+  .name("l2m")
+  .usage('[options] <file>')
+  .version('1.0.0')
+  .option('-v, --value-position, <position>', '値を表す列の位置を指定します', /^\d+$/i, "2")
+  .option('-x, --x-position, <position>', '横方向のラベルを表す列の位置を指定します', /^\d+$/i, "1")
+  .option('-y, --y-position <position>', '縦方向のラベルを表す列の位置を指定します', /^\d+$/i, "0")
+  .option('--y-order <order>', '縦方向のラベルの並び順を指定します', /^(asc|desc)$/i, null)
+  .option('--x-order <order>', '横方向のラベルの並び順を指定します', /^(asc|desc)$/i, null)
+  .option('--na-as <value>}', 'N/Aの箇所の表示内容を指定します', '')
+  .arguments('<file>')
+  .parse(process.argv);
+
+async function main() {
   const path = process.argv[2];
   const content = await readFileAsync(path, "utf8");
   const csvLines = await parseCsvAsync(content);
@@ -22,4 +37,5 @@ const stringifyCsvAsync = util.promisify<any, any>(stringify);
   }).build();
   const result = await stringifyCsvAsync(matrix);
   process.stdout.write(result);
-})();
+};
+main();
