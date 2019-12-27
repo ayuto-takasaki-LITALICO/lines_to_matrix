0e766205b58b0de19ea50a014dc463bf14a0435a
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2019-12-27 14:08:06 +0900

    :pencil2: コマンドライン引数から値を受け取るようにした

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import commander from "commander";
 import readline from "readline";
 import { MatrixBuilder } from "./matrix_builder";
 
-const parseCsvAsync = util.promisify<any, any>(parse);
+const parseCsvAsync = util.promisify<any, any, any>(parse);
 const stringifyCsvAsync = util.promisify<any, any>(stringify);
 
 const readFileAsync = util.promisify(fs.readFile);
@@ -25,58 +25,80 @@ const readAllLinesAsync = (): Promise<string[]> => {
   });
 };
 
-const program = new commander.Command()
-  .name("l2m")
-  .usage("[options] <file>")
-  .version("1.0.0")
-  .option(
-    "-y, --y-position <position>",
-    "縦方向のラベルを表す列の位置を指定します",
-    /^\d+$/i,
-    "0"
-  )
-  .option(
-    "-x, --x-position, <position>",
-    "横方向のラベルを表す列の位置を指定します",
-    /^\d+$/i,
-    "1"
-  )
-  .option(
-    "-v, --value-position, <position>",
-    "値を表す列の位置を指定します",
-    /^\d+$/i,
-    "2"
-  )
-  .option(
-    "--y-order <order>",
-    "縦方向のラベルの並び順を指定します",
-    /^(asc|desc)$/i,
-    null
-  )
-  .option(
-    "--x-order <order>",
-    "横方向のラベルの並び順を指定します",
-    /^(asc|desc)$/i,
-    null
-  )
-  .option("--na-as <value>}", "N/Aの箇所の表示内容を指定します", "")
-  .arguments("<file>")
-  .parse(process.argv);
+const initL2mCommand = (): commander.Command => {
+  return new commander.Command()
+    .name("l2m")
+    .usage("[options] <file>")
+    .version("1.0.0")
+    .option(
+      "-y, --y-position <position>",
+      "縦方向のラベルを表す列の位置を指定します",
+      /^\d+$/i,
+      "0"
+    )
+    .option(
+      "-x, --x-position, <position>",
+      "横方向のラベルを表す列の位置を指定します",
+      /^\d+$/i,
+      "1"
+    )
+    .option(
+      "-v, --value-position, <position>",
+      "値を表す列の位置を指定します",
+      /^\d+$/i,
+      "2"
+    )
+    .option(
+      "--y-order <order>",
+      "縦方向のラベルの並び順を指定します",
+      /^(asc|desc)$/i,
+      null
+    )
+    .option(
+      "--x-order <order>",
+      "横方向のラベルの並び順を指定します",
+      /^(asc|desc)$/i,
+      null
+    )
+    .option("--na-as <value>}", "N/Aの箇所の表示内容を指定します", "")
+    .option(
+      "--from-line <number>",
+      "CSVの解析を始める行番号を指定します",
+      /^\d+$/i,
+      "1"
+    )
+    .arguments("<file>");
+};
 
-async function main() {
-  const fileName = program.args[0];
+const importCsv = async (
+  fileName: string,
+  { fromLine = 1 } = {}
+): Promise<any[][]> => {
   const content = fileName
     ? await readFileAsync(fileName, "utf8")
     : (await readAllLinesAsync()).join("\n");
-  const csvLines = await parseCsvAsync(content);
-  const matrix = new MatrixBuilder(csvLines, {
-    yPosition: 0,
-    xPosition: 1,
-    valuePosition: 2,
-    xOrder: "asc",
-    yOrder: "asc",
-    naAs: ""
-  }).build();
+  return parseCsvAsync(content, { from_line: fromLine });
+};
+
+const commanderProgramToOptions = (program: commander.Command) => {
+  return {
+    yPosition: Number(program.yPosition),
+    xPosition: Number(program.xPosition),
+    valuePosition: Number(program.valuePosition),
+    xOrder: program.xOrder,
+    yOrder: program.yOrder,
+    naAs: program.naAs
+  };
+};
+
+async function main() {
+  const program = initL2mCommand().parse(process.argv);
+  const fileName = program.args[0];
+  const fromLine = Number(program.fromLine);
+  const matrix = new MatrixBuilder(
+    await importCsv(fileName, { fromLine }),
+    commanderProgramToOptions(program)
+  ).build();
   const result = await stringifyCsvAsync(matrix);
   process.stdout.write(result);
 }
