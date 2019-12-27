9d2a25831e1a7a85f37da1e1df52c5142442cb82
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2019-12-27 14:35:34 +0900

    :pencil2: 無駄な変数消した

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,12 @@ import { exportCsvAsync } from "./utils/csv_utils";
 
 (async function main() {
   const program = createL2mCommand().parse(process.argv);
-  const matrix = new MatrixBuilder(
-    await importSource(program),
-    commandToOptions(program)
-  ).build();
-  process.stdout.write(await exportCsvAsync(matrix));
+  process.stdout.write(
+    await exportCsvAsync(
+      new MatrixBuilder(
+        await importSource(program),
+        commandToOptions(program)
+      ).build()
+    )
+  );
 })();
