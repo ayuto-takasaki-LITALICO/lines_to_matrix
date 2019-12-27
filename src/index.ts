c3de0f510ffad1357450ee559770a4d461c4da41
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2019-12-27 09:36:35 +0900

    :pencil2: クラス切り出した

--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,12 @@
-import _ from "lodash";
 import stringify from "csv-stringify";
 import parse from "csv-parse";
 import fs from "fs";
 import util from "util";
+import { MatrixBuilder } from "./matrix_builder";
 
 const readFileAsync = util.promisify(fs.readFile);
 const parseCsvAsync = util.promisify<any, any>(parse);
 const stringifyCsvAsync = util.promisify<any, any>(stringify);
-
-type MatrixCellData = string | number | boolean | null;
-type MatrixRowData = MatrixCellData[];
-type MatrixData = MatrixRowData[];
-type MatrixBuilderOptions = {
-  yPosition: number;
-  xPosition: number;
-  valuePosition: number;
-  xOrder?: Order;
-  yOrder?: Order;
-  naAs: string;
-};
-type Order = "asc" | "desc";
-
-class MatrixBuilder {
-  matrix: MatrixData;
-
-  X: MatrixRowData;
-
-  Y: MatrixRowData;
-
-  source: MatrixData;
-
-  options: MatrixBuilderOptions;
-
-  constructor(source: MatrixData, options: MatrixBuilderOptions) {
-    this.source = source;
-    this.options = options;
-    this.matrix = [];
-    this.X = [];
-    this.Y = [];
-  }
-
-  build(): MatrixData {
-    this.init();
-    this.sortLabels();
-    this.readLines();
-    return this.exportWithHeader();
-  }
-
-  init(): void {
-    this.X = _.uniq(_.map(this.source, this.options.xPosition));
-    this.Y = _.uniq(_.map(this.source, this.options.yPosition));
-    this.matrix = _.times(this.Y.length, () =>
-      new Array(this.X.length).fill(this.options.naAs)
-    );
-  }
-
-  sortLabels(): void {
-    if (this.options.xOrder) {
-      if (this.options.xOrder === 'asc') {
-        _.sortBy(this.X);
-      } else {
-        _.sortBy(this.X);
-        _.reverse(this.X);
-      }
-    }
-    if (this.options.yOrder) {
-      if (this.options.yOrder === 'asc') {
-        _.sortBy(this.Y);
-      } else {
-        _.sortBy(this.Y);
-        _.reverse(this.Y);
-      }
-    }
-  }
-
-  readLines(): void {
-    this.source.forEach(row => {
-      const yTitle = row[this.options.yPosition];
-      const xTitle = row[this.options.xPosition];
-      const value = row[this.options.valuePosition];
-      this.matrix[this.Y.indexOf(yTitle)][this.X.indexOf(xTitle)] = value;
-    });
-  }
-
-  exportWithHeader(): MatrixData {
-    return [
-      [null, ...this.X],
-      ...this.matrix.map((it, index) => [this.Y[index], ...it])
-    ];
-  }
-}
 
 (async function main() {
   const path = process.argv[2];
