2a2947844d9c9ddd1c6b9aca5ca29439b02448b6
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-06-13 15:00:52 +0900

    :pencil2: ルール変更

--- src/utils/csv_utils.ts
+++ src/utils/csv_utils.ts
@@ -2,6 +2,8 @@ import util from "util";
 import stringify from "csv-stringify";
 import parse from "csv-parse";
 
+/* eslint @typescript-eslint/no-explicit-any: 0 */
+
 const parseCsvAsync = util.promisify<any, any, any>(parse);
 const stringifyCsvAsync = util.promisify<any, any>(stringify);
 
